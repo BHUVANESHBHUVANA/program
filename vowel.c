1bc3cbeeb5b6663f2944b47a22409b31231a0eea
BHUVANESHBHUVANA <36880042+BHUVANESHBHUVANA@users.noreply.github.com>
2018-08-06 12:13:06 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,24 +3,24 @@ void main()
 {
 char n;
 printf("\nEnter the character:");
-scanf(%s",n);
-if((n=='a')(n=='A'))
+scanf("%s",n);
+if((n=='a')||(n=='A'))
 {
 printf("\nThe vowel is A");
 }
-else if((n=='e')(n=='E'))
+else if((n=='e')||(n=='E'))
 {
 printf("\nThe vowel is E");
 }
-else if((n=='i')(n=='I'))
+else if((n=='i')||(n=='I'))
 {
 printf("\nThe vowel is I");
 }
-else if((n=='o')(n=='O'))
+else if((n=='o')||(n=='O'))
 {
 printf("\nThe vowel is O");
 }
-else if((n=='u')(n=='U'))
+else if((n=='u')||(n=='U'))
 {
 printf("\nThe vowel is U");
 }

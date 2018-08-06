9a5dcfcada3e004bacf668e6f6fd6a91e70c8e8e
BHUVANESHBHUVANA <36880042+BHUVANESHBHUVANA@users.noreply.github.com>
2018-08-06 12:05:17 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+char n;
+printf("\nEnter the character:");
+scanf(%s",n);
+if((n=='a')(n=='A'))
+{
+printf("\nThe vowel is A");
+}
+else if((n=='e')(n=='E'))
+{
+printf("\nThe vowel is E");
+}
+else if((n=='i')(n=='I'))
+{
+printf("\nThe vowel is I");
+}
+else if((n=='o')(n=='O'))
+{
+printf("\nThe vowel is O");
+}
+else if((n=='u')(n=='U'))
+{
+printf("\nThe vowel is U");
+}
+else
+{
+printf("\nThe character is invalid");
+}
+getch();
+}
